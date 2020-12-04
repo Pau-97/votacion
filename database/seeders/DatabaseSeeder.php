d2a1eb347b7e1a20dc5be2da102c5d5a778c7678
Paulo Carbajal <paulo@ludik.pe>
2020-12-03 19:19:23 -0500

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesUser::class,
             UserSeeder::class,
+            TemporadaSeeder::class,
+            CandidatoSeeder::class,
         ]);
     }
 }
